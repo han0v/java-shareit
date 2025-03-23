7bcbc8c6360022c4fbfab7d85d028dae938e1ef3
faiz <hanovfaiz@yandex.ru>
2025-03-23 12:58:03 +0500

    рефакторинг контроллеров и мапперов

--- server/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingState;
